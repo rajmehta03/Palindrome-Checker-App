936d4a253300fc0bbdb2f58b05c95bd685b92a35
rajmehta03 <mraj54131@gmail.com>
2026-02-12 10:26:06 +0530

    UC1: Application Entry & Welcome Message

---
+++ src/PalindromeCheckerApp.java
@@ -0,0 +1,6 @@
+public class PalindromeCheckerApp {
+    public static void main(String[] args){
+        System.out.println("Jai Shree Ram , Welcome to Palindrome Checker Management App\n" +
+                "Version : 1.0"+"\nSystem initialized successfully");
+    }
+}
