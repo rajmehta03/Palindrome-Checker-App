40ea345ff369d18ff3ba355d15515bbe0007d601
rajmehta03 <mraj54131@gmial.com>
2026-03-03 22:49:12 +0530

    done use case 9

---
+++ Downloads/Palindrome-Checker-App-main/Palindrome-Checker-App-main/src/PalindromeCheckerApp.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class PalindromeCheckerApp {
+
+    public static boolean isPalindrome(String str, int start, int end) {
+        if (start >= end) {
+            return true;
+        }
+
+        if (str.charAt(start) != str.charAt(end)) {
+            return false;
+        }
+
+        return isPalindrome(str, start + 1, end - 1);
+    }
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        String input = scanner.nextLine();
+
+        input = input.replaceAll("\\s+", "").toLowerCase();
+
+        boolean result = isPalindrome(input, 0, input.length() - 1);
+
+        if (result) {
+            System.out.println("The given string is a Palindrome.");
+        } else {
+            System.out.println("The given string is NOT a Palindrome.");
+        }
+
+        scanner.close();
+    }
+}
